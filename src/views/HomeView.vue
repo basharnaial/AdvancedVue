0fb7b4d4b04266d34e4054fb52cfb72fb5668255
Bashar Naial <4310757@upm.edu.sa>
2025-07-22 13:40:20 +0300

    [18]Composition API Two Mental Leaps to Script Setup Summary - The Options API is familiar, groups logic by option, and is fully supported. - The Composition API offers more flexibility, explicit reactivity, and better code reuse. - Use ref to declare reactive state; access .value in JS, but not in templates. - <script setup> simplifies component syntax by removing boilerplate. - Reactivity transform is an experimental feature to reduce .value usage. ## You can choose either API based on your project needs and comfort level. Both are valid and supported ways to build Vue components.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import {  ref } from 'vue'
+
+
+let message = ref('hello bashar');
+
+setTimeout(() =>{
+  message.value = 'i have been changed';
+}, 2000);
+
+
+let doSomething = () =>{ 
+  alert('doSomething');
+}
 </script>
 
 <template>
+
   <main>
     <TheWelcome />
   </main>
+  <input type="text" v-model="message">
+  <button @click="doSomething">doSomething</button>
+  {{ message }}
+
 </template>
