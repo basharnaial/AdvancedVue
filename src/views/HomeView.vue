e79da289cae16251dc1d13833d38ff95bc7f4ac9
Bashar Naial <4310757@upm.edu.sa>
2025-07-23 08:45:39 +0300

    [21] CompAPI Refactor to defineProps and defineEmits alt + j (to select duplicated word) ctrl + shift + alt + j (to select all duplication one time) Summary ⦁Use ref to access DOM nodes in the Composition API. ⦁Use Vue's event modifiers like .tab and .prevent to simplify event handling. ⦁To support v-model in custom components with script setup, define props with modelValue and declare emitted events with defineEmits. ⦁Emit update:modelValue events to keep parent and child in sync. ⦁Extract reusable logic into components for maintainability. This pattern enhances developer experience when building rich text inputs or code editors with Vue 3's Composition API.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,16 @@
 <script setup>
-import { useStorage } from "@/composables/useStorage";
+import TabbableTextarea from "@/components/TabbableTextarea.vue";
+import { ref } from "vue";
 
-let food = useStorage('food', 'tacos');
-let age = useStorage('age', 23);
+
+let comment = ref('initial textarea value');
 
 </script>
 
 <template>
-
-<main>
-  <p>
-  what is your fav food?
-    <input type="text" v-model="food" >
-  </p>
-
-<p>
-  how old are you?
-  <input type="text" v-model="age">
-</p>
-
-</main>
+  <main>
+    <form>
+      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px;" />
+    </form>
+  </main>
 </template>
